0d95267cef074bdbc3d6a9487d1708c258142676
garrettruss <garrettmruss@gmail.com>
2021-03-02 16:03:15 -0500

    afternoon updates

--- README.md
+++ README.md
@@ -1,2 +1,26 @@
-##
-Goals - display more than one park, by park name, flip through pages of parks when applicable. 
+# Nationals Parks Preview
+
+a simple web app that shows NPS properties based on keyword or state abbreviation. 
+
+## Technologies Used
+HTML5
+CSS3
+JS
+Jquery
+NPS API https://www.nps.gov/subjects/developer/get-started.htm 
+Git
+Github
+Github Pages (for deployment)
+
+## Screenshots
+
+![screenshot 1](./imgs/shot1.png)
+![screenshot 2](./imgs/shot2.png)
+
+## Getting Started:
+[click](https://garrettruss.github.io/NPS-Project/) to see the app. 
+
+## Future Enhancements
+Display more than one result. 
+Impliment pagination.
+Impliment modar or card desk system to display more than one result. 
--- js/script.js
+++ js/script.js
@@ -41,6 +41,7 @@ function handleSubmit(evt) {
 
 //Display results. Right now the first '0' array is selected tos show the APi is linked. Ideally want to display first 40 results in a list. 
 
+
 function render() {
         $fullName.text(parkData.data[0].fullName);
         $states.text(parkData.data[0].states);
@@ -52,12 +53,99 @@ function render() {
        ;
     }
 
+    /*
+ const cards = parkData.results.slice(10).map(function (parks) {
+             return `
+                <article data-url="${parkData.url}" class="card">
+                    <h3>${parkData.fullName}</h3>
+                </article>
+            `;
+ });
+*/ 
+
+   /*
+    const cards = parkData.results.map(function (parks) {
+             return `
+                <article data-url="${parks.url}" class="card">
+                    <h3>${parks.fullName}</h3>
+                </article>
+            `;
+ });
+    
+    $collection.html(cards);
+
+    */
+/*
+let parks = render.map;
+console.log(parks);
+*/
+
+//array.map(function(currentValue, index, arr), thisValue)
+
+/*
+var array = [
+    {id: 0, name: 'John', age: 20},
+    {id: 1, name: 'Jane', age: 22},
+    {id: 2, name: 'Bob', age: 24},
+    {id: 3, name: 'Ana', age: 26},
+];
+
+var newArray = myArray.map(function(profile, index, myArr) {
+    var newProfile = {
+        'id': index,
+        'name': profile.firstName,
+        'age': profile.age
+    }
+
+    return newProfile
+})
+
+newArray.forEach(function(profile, index, myArr) {
+    console.log(profile.fullName)
+});
+
+/*
+Output:
+    John
+    Jane
+    Bob
+    Ana
+*/
+
+/*
+const results = parkData.results.map(function (park) {
+    return {
+            $fullName.text(parkData.data[].fullName);
+            $states.text(parkData.data[].states);
+            $designation.text(parkData.data[].designation);
+            $description.text(parkData.data[].description);
+            $directionsInfo.text(parkData.data[].directionsInfo);
+            $weatherInfo.text(parkData.data[].weatherInfo);
+            $moreInfo.text(parkData.data[].url;
+
+            $collection.html(cards);
+        } 
+
+  */
+ 
+  
+/*
+let newList = parkData.map(selParks);
+
+document.getElementsByClassName('Results-List').innerHTML = newList;
+
+function selParks (value, index, array) {
+    return 
+}
+*/
 
 /*
 //Trying to iterate over the array to display up to the limit of 40 results
+
+
 function render() {
-    //for (let i = 0; i < data; i++) 
-    {
+
+    if (parkData) {
         $fullName.text(parkData.data[i].fullName);
         $designation.text(parkData.data[i].designation);
         $description.text(parkData.data[i].description);
