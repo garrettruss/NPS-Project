c56f897c4c41e8fb0507e61a823f21305cd5cbbf
garrettruss <garrettmruss@gmail.com>
2021-03-03 21:19:17 -0500

    removing modal

--- js/script.js
+++ js/script.js
@@ -9,13 +9,12 @@ const BASE_URL = "https://developer.nps.gov/api/v1/parks?q=";
 
 /*----- app's state (variables) -----*/
 let parkData; 
+// detailData;
 
 /*----- cached element references -----*/
 
 const $collection = $('#collection'); 
-const $modal = $('.modal');
-
-
+// const $modal = $('.modal');
 let $input = $('input[type="text"]');
 
 // Displayed on card 
@@ -27,25 +26,33 @@ let $designation = $('#designation');
 let $description = $('#description');
 let $directionsInfo = $('#directionsInfo');
 let $weatherInfo = $('#weatherInfo');
-let $moreInfo = $('#moreInfo');
+let $url = $('#url');
 
 /*----- event listeners -----*/
 
 $('form').on("submit", handleSubmit);
-$collection.on('click', 'article.card', handeClick);
+// $collection.on('click', 'article.card', handleClick); 
 
 /*----- functions -----*/
+/*
+function handleClick() {
+        console.log(this.dataset)
+        $modal.modal();
+    }
+*/
+
 
 // Take input from user to search NPS API for results, render results for user or show error. 
 function handleSubmit(evt) {
     evt.preventDefault();
     const term = $input.val();
     $input.val("");
-    $.ajax(BASE_URL + term + "&limit=30&api_key=" + API_KEY)
+    $.ajax(BASE_URL + term + "&limit=40&api_key=" + API_KEY)
     .then(function(data) {
     console.log('Park Data ', data);
     parkData = data;
     render();
+
     }, function(error) {
     console.log('error ', error);
      alert("Sorry about that!");
@@ -56,35 +63,54 @@ function handleSubmit(evt) {
 function render() {
     const cards = parkData.data.map(function(park) {
            return `
-            <article data-info="${park.data}" class="card">
-                <h3>${park.fullName}</h3>
+            <article data-info="${parkData}" class="card">
+                <h3 = name>${park.fullName}</h3>
+                br
+    
+                <p>${park.designation}</p>
+                <p>${park.description}</p>
+                <p>${park.directionsInfo}</p>
+                <p>${park.weatherInfo}</p>
+                <p>${park.url}</p>
             </article>
             `;
     });
     $collection.html(cards);
 }
+  
 
 
-    
+/*
 // V1
+
+
+   $states.text(data.states);
+        $designation.text(data.designation);
+        $description.text(data.designation);
+        $directionsInfo.text(directionsInfo);
+        $weatherInfo.text(weatherInfo);
+        $moreInfo.text(moreInfo);
+
+
 //Display park info in popup of card. 
-function handeClick() {
-    $.ajax(this.dataset)
-    .then(function(data) {
-        $states.text(data.states);
+function handeClick(evt) {
+        $modal.modal();
+    } function(error) {
+        console.log(error);
+    };
+*/ 
+
+/*
+     $.ajax(this.dataset)
+          $states.text(data.states);
         $designation.text(data.designation);
         $description.text(data.designation);
         $directionsInfo.text(directionsInfo);
         $weatherInfo.text(weatherInfo);
         $moreInfo.text(moreInfo);
         console.log(data)
-        $modal.modal();
-    }, function(error) {
-        console.log(error);
-    });
-}
+    .then(function(data) {
 
-/*
 //V2 
 //Display park info in popup of card. 
 function handeClick() {
